6e8ddec96d3b5ed1b5148d8181b39b71cafb919b
platers <platers81@gmail.com>
2021-06-04 23:38:09 -0400

    remove number layer and add shift+alt

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/victor/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/victor/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTRL, KC_Z, KC_X, KC_C, KC_V, KC_B, 											KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
 		KC_UP, KC_DOWN, 														KC_LEFT, KC_RGHT, 
 		LT(_SYMBOLS,KC_ENT), KC_SPC, 											LSFT_T(KC_BSPC), LT(_MEDIA,KC_ESC), 
-		KC_LALT, KC_LGUI, 														LT(_NUMBERS, KC_DEL), KC_LGUI, 
+		KC_LALT, KC_LGUI, 														LSA(KC_DEL), KC_LGUI, 
 		KC_LGUI, TG(_WINDOWS), 														KC_APP, KC_RALT),
 	[_WINDOWS] = LAYOUT_5x6(
 		_______, _______, _______, _______, _______, _______, 				_______, _______, _______, _______, _______, _______, 
@@ -140,15 +140,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       _______,_______,                        _______, _______,
                                       TG(_QWERTY),_______,                     _______,  _______,
                                       _______, _______,                        _______, _______),
-	[_NUMBERS] = LAYOUT_5x6(
-		KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,			 							KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, 
-		KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, 								KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, 
-		_______, KC_1, KC_2, KC_3, KC_4, KC_5, 											KC_6, KC_7, KC_8, KC_9, KC_0, _______, 
-		_______, _______, _______, _______, _______, _______, 					_______, _______, _______, _______, _______, _______, 
-		_______, _______, 																_______, _______,
-		_______, _______, 																_______, _______, 
-		_______, _______, 															_______, _______, 
-		_______, _______, 																_______, _______),
 };
 
 // Light LEDs 9 & 10 in cyan when keyboard layer 1 is active
